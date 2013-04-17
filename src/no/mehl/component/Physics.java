5d01b7debf75a7d906ced14dc52757f6bdfe6a4e
Kjetil Mehl <aspic@mehl.no>
2013-04-17 21:19:40 +0200

    Physics is properly updated.

--- src/no/mehl/component/Physics.java
+++ src/no/mehl/component/Physics.java
@@ -17,7 +17,7 @@ public abstract class Physics extends Component {
 
 	// Fields
 	protected Body body;
-	protected Vector3 position;
+	protected Vector3 position = new Vector3();
 	protected Vector3 velocity = new Vector3();
 	protected Vector3 lastVel = new Vector3();
 	protected Vector3 lastPos = new Vector3();
@@ -78,7 +78,6 @@ public abstract class Physics extends Component {
 	
 	/** Returns the current velocity for this component */
 	public Vector3 getVelocity() {
-		if(this.velocity == null) this.velocity = new Vector3();
 		return body != null ? this.velocity.set(body.getLinearVelocity().x, body.getLinearVelocity().y, this.velocity.z) : this.velocity;
 	}
 	
@@ -90,7 +89,6 @@ public abstract class Physics extends Component {
 	
 	/** Returns the current position for this component, or the initial position. **/
 	public Vector3 getPosition() {
-		if(this.position == null) this.position = new Vector3();
 		return body != null ? this.position.set(body.getPosition().x, body.getPosition().y, this.position.z) : this.position;
 	}
 	
@@ -119,10 +117,16 @@ public abstract class Physics extends Component {
 	
 	/** Update the position and angle for this body */
 	public void updateTransform(Vector3 pos, float angle) {
-		this.position = pos != null ?  new Vector3(pos) : new Vector3();
+//		this.position = pos != null ?  new Vector3(pos) : new Vector3();
 		
-		if(this.body != null) {
-			this.body.setTransform(position.x, position.y, angle);
+		// Could be null
+		if(pos == null) {
+			this.position.set(body.getPosition().x, body.getPosition().y, position.z);
+		} else {
+			this.position.set(pos);
+			if(this.body != null) {
+				this.body.setTransform(position.x, position.y, angle);
+			}
 		}
 		this.angle = angle;
 	}
@@ -185,7 +189,7 @@ public abstract class Physics extends Component {
 	 */
 	public Physics fill(Snapshot snapshot) {
 		angle = snapshot.f_0 != null ? snapshot.f_0.get() : angle;
-		if(snapshot.v3_0 != null) updateTransform(snapshot.v3_0, angle);
+		updateTransform(snapshot.v3_0 != null ? snapshot.v3_0 : position.set(body.getPosition().x, body.getPosition().y, position.z), angle);
 		if(snapshot.v3_1 != null) updateVelocity(snapshot.v3_1.x, snapshot.v3_1.y, snapshot.v3_1.z);
 		if(snapshot.v2_0 != null) updateForce(snapshot.v2_0.x, snapshot.v2_0.y);
 		if(snapshot.d_0 != null) setDimension(snapshot.d_0);
