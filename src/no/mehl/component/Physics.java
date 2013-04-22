04635cca0936b4df6a7968178df8adab3e7af4c4
Kjetil Mehl <aspic@mehl.no>
2013-04-22 19:08:42 +0200

    Split physics into a box2d component.

--- src/no/mehl/component/Physics.java
+++ src/no/mehl/component/Physics.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Filter;
 
 import no.mehl.libgdx.utils.Compare;
 import no.mehl.libgdx.utils.Dimension;
@@ -17,23 +16,19 @@ import no.mehl.libgdx.utils.Mutable;
 public abstract class Physics extends Component {
 
 	// Fields
-	protected Body body;
+//	protected Body body;
 	protected Vector3 position = new Vector3();
 	protected Vector3 velocity = new Vector3();
 	protected Vector3 lastVel = new Vector3();
 	protected Vector3 lastPos = new Vector3();
+	protected Vector3 impulse = new Vector3();
 	protected Vector3 toPos;
 	protected float angle;
-	protected UserData data;
 	
-	protected Vector2 force = new Vector2();
-	
+//	protected Vector2 force = new Vector2();
 	protected Dimension dim;
 	protected Snapshot snapshot = new Snapshot();
 	protected Snapshot dS = new Snapshot();
-	
-	protected float gravityZ = 0f;
-	protected static final float GRAV_Z = -25;
 	
 	/** Loads the client specification, unless overridden */
 	@Override
@@ -41,69 +36,41 @@ public abstract class Physics extends Component {
 		loadClient(entity);
 	}
 	
+	/** Will mark this component as changed */
 	@Override
 	public void runServer(GameEntity entity, float delta) {
 		setChanged();
 	}
-
-	@Override
-	public void runClient(GameEntity entity, float delta) {
-//		if(this.snapshot != null) {
-//			fill(this.snapshot);
-//		}
-	}
 	
+	public void setAngle(float angle) {
+		this.angle = angle;
+	}
+
 	/** Returns the angle of the body in radians */
 	public float getAngle() {
-		if(body != null) {
-			angle = body.getAngle();
-		}
 		return angle;
 	}
 	
-	public String toString() {
-		return getClass().getSimpleName();
-	}
-	
-	public void applyForce(float forceX, float forceY) {
-		if(force == null) force = new Vector2();
-		body.applyForceToCenter(force.set(forceX, forceY), true);
-	}
-	
-	/** Sets the direction for this shape **/
-	public abstract void updateFixture();
-	protected abstract BodyDef createBodyDef();
-	
 	/** Applies angular impulse to body */
 	public void rotateBy(float rot) {
-		if(this.body != null) this.body.applyAngularImpulse(rot, true);
 	}
 	
 	/** Returns the current velocity for this component */
 	public Vector3 getVelocity() {
-		return body != null ? this.velocity.set(body.getLinearVelocity().x, body.getLinearVelocity().y, this.velocity.z) : this.velocity;
+		return this.velocity;
 	}
 	
-	public Vector2 getForce() {
-		Vector2 tempForce = new Vector2(force);
-		force.set(0, 0);
-		return tempForce;
+	public Vector3 getImpulse() {
+		return this.impulse;
 	}
 	
 	/** Returns the current position for this component, or the initial position. **/
 	public Vector3 getPosition() {
-		return body != null ? this.position.set(body.getPosition().x, body.getPosition().y, this.position.z) : this.position;
+		return this.position;
 	}
 	
 	public void setDimension(Dimension dim) {
 		this.dim = new Dimension(dim);
-			
-		if(this.body != null) {
-			for (int i = 0; i < body.getFixtureList().size(); i++) {
-				body.destroyFixture(body.getFixtureList().get(i));
-			}
-			updateFixture();
-		}
 	}
 	
 	public Dimension getDimension() {
@@ -114,55 +81,42 @@ public abstract class Physics extends Component {
 	/** Updates and returns the {@link Snapshot} for this component. */
 	@Override
 	public Snapshot getSnapshot(boolean delta) {
-		setSynced();
 		return delta ? get() : getFull();
 	}
 	
 	/** Update the position and angle for this body */
-	public void updateTransform(Vector3 pos, float angle) {
-		// Could be null
-		if(pos == null) {
-			this.position.set(body.getPosition().x, body.getPosition().y, position.z);
-		} else {
-			this.position.set(pos);
-			if(this.body != null) {
-				this.body.setTransform(position.x, position.y, angle);
-			}
-		}
+	public void updateTransform(float x, float y, float z, float angle) {
+		this.position.set(x, y, z);
 		this.angle = angle;
 	}
 	
 	/** Update the velocity for this body */
-	private void updateVelocity(float x, float y, float z) {
-		if(this.body != null && x != 0 && y != 0) {
-			this.body.setLinearVelocity(x, y);
-		}
+	protected void updateVelocity(float x, float y, float z) {
 		this.velocity.set(x, y, z);
 	}
 	
-	private void updateForce(float x, float y) {
-		if(this.body != null && (x != 0 || y != 0)) {
-			this.body.applyForceToCenter(x, y, true);
-		}
+	/** Sets the given impulse to this body */
+	public void updateImpulse(float x, float y, float z) {
+		this.impulse.set(x, y, z);
 	}
 	
 	public Snapshot get() {
 		if(dS.d_0 == null) dS.d_0 = new Dimension();
 		if(dS.v3_0 == null) dS.v3_0 = new Vector3();
 		if(dS.v3_1 == null) dS.v3_1 = new Vector3();
+		if(dS.v3_2 == null) dS.v3_2 = new Vector3();
 		if(dS.v2_0 == null) dS.v2_0 = new Vector2();
 		if(dS.f_0 == null) dS.f_0 = new Mutable.Float(0);
 		
 		snapshot.id = getId();
 		
 		snapshot.d_0 = Compare.dimension(dS.d_0, getDimension());
+		
 		snapshot.v3_0 = Compare.vector(dS.v3_0, getPosition());
 		snapshot.v3_1 = Compare.vector(dS.v3_1, getVelocity());
-		
-		snapshot.v2_0 = Compare.vector(dS.v2_0, getForce());
+		snapshot.v3_2 = Compare.vector(dS.v3_2, getImpulse());
+
 		snapshot.f_0 = Compare.mutableFloat(dS.f_0, getAngle());
-		
-		snapshot.data = data.retrieve();
 		
 		return snapshot.validate();
 	}
@@ -171,13 +125,12 @@ public abstract class Physics extends Component {
 		snapshot.id = getId();
 		
 		snapshot.d_0 = getDimension();
+		
 		snapshot.v3_0 = getPosition();
 		snapshot.v3_1  = getVelocity();
-		snapshot.v2_0 = getForce();
+		snapshot.v3_2 = getImpulse();
 		
 		snapshot.f_0 = new Mutable.Float(getAngle());
-		
-		snapshot.data = data;
 		
 		return snapshot;
 	}
@@ -189,94 +142,20 @@ public abstract class Physics extends Component {
 	 */
 	public Physics fill(Snapshot snapshot) {
 		angle = snapshot.f_0 != null ? snapshot.f_0.get() : angle;
-		updateTransform(snapshot.v3_0 != null ? snapshot.v3_0 : position.set(body.getPosition().x, body.getPosition().y, position.z), angle);
+		if(snapshot.v3_0 != null) updateTransform(snapshot.v3_0.x, snapshot.v3_0.y, snapshot.v3_0.z, angle);
 		if(snapshot.v3_1 != null) updateVelocity(snapshot.v3_1.x, snapshot.v3_1.y, snapshot.v3_1.z);
-		if(snapshot.v2_0 != null) updateForce(snapshot.v2_0.x, snapshot.v2_0.y);
+		if(snapshot.v2_0 != null) updateImpulse(snapshot.v3_2.x, snapshot.v3_2.y, snapshot.v3_2.z);
 		if(snapshot.d_0 != null) setDimension(snapshot.d_0);
-		if(snapshot.data != null) setUserdata(snapshot.data);
 		
-		this.snapshot = snapshot;
 		return this;
 	}
 	
 	@Override
 	public void destroy(GameEntity entity) {
-		entity.getWorld().destroyBody(body);
+		
 	}
 	
-	public void setUserdata(int id) {
-		this.body.setUserData(id);
-	}
-
 	public Vector3 getToPos() {
 		return this.toPos;
-	}
-
-	public Body getBody() {
-		return this.body;
-	}
-
-	public void setGravityZ(float grav) {
-		this.gravityZ = grav;
-		if(grav == 0) this.velocity.z = 0;
-	}
-	
-	public void setPosZ(float z) {
-		this.position.z = z;
-	}
-	
-	public void setUserdata(UserData object) {
-		// Set
-		this.data = object;
-		
-		// Update
-		if(this.body != null) {
-			Filter filter = null;
-			if(data.contains(UserData.D_FILTER)) {
-				filter = (Filter) data.get(UserData.D_FILTER);
-			}
-			
-			for (int i = 0; i < body.getFixtureList().size(); i++) {
-				body.getFixtureList().get(i).setUserData(object);
-				if(filter != null) body.getFixtureList().get(i).setFilterData(filter);
-			}
-			this.body.setUserData(object);
-		}
-	}
-	/** Set a new filter for the fixtures. Will transmit changes */
-	public void setFilterData(Filter filter) {
-		this.data.put(UserData.D_FILTER, filter);
-		
-		if(this.body != null) {
-			for (int i = 0; i < body.getFixtureList().size(); i++) {
-				body.getFixtureList().get(i).setFilterData(filter);
-			}
-		}
-	}
-	
-	/** Load common data for bodies */
-	protected void loadBody(GameEntity entity) {
-		if(data != null) setUserdata(data.load(entity, this));
-		
-		updateTransform(position, angle);
-		setDimension(dim);
-	}
-
-	public void applyImpulse(float x, float y) {
-		if(this.body != null) {
-			this.body.applyLinearImpulse(x, y,  body.getPosition().x, body.getPosition().y, true);
-		}
-	}
-
-	public void setSensor(boolean b) {
-		if(this.body != null) {
-			for (int i = 0; i < body.getFixtureList().size(); i++) {
-				body.getFixtureList().get(i).setSensor(b);
-			}
-		}
-	}
-
-	public void setAngle(float angle) {
-		this.angle = angle;
 	}
 }
---
+++ src/no/mehl/component/physics/Box2DPhysics.java
@@ -0,0 +1,290 @@
+package no.mehl.component.physics;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
+import com.badlogic.gdx.physics.box2d.Filter;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Shape;
+
+import no.mehl.component.Component;
+import no.mehl.component.GameEntity;
+import no.mehl.component.Physics;
+import no.mehl.component.Snapshot;
+import no.mehl.component.UserData;
+import no.mehl.libgdx.utils.Dimension;
+
+/**
+ * This component definition is tightly coupled to the {@link Body} class in Box2D.
+ * @author Kjetil Mehl <kjetil@mehl.no>
+ */
+public class Box2DPhysics extends Physics {
+	
+	public static final String TYPE_CIRCLE = "circle";
+	public static final String TYPE_RECT = "rect";
+	public static final String TYPE_EDGE = "edge";
+
+	// Fields
+	private Body body;
+	private UserData data;
+	
+	protected float gravityZ = 0f;
+	public static final float GRAV_Z = -25;
+	
+	public Box2DPhysics() {
+		this(new UserData());
+	}
+	
+	public Box2DPhysics(UserData data) {
+		this(data, new Vector2(), new Dimension());
+	}
+	
+	public Box2DPhysics(UserData data, Vector2 position, Dimension dimension) {
+		this(data, new Vector3(position.x, position.y, 0), dimension);
+	}
+	
+	public Box2DPhysics(UserData data, Vector3 position, Dimension dimension) {
+		this.position = position;
+		this.dim = dimension;
+		this.data = data;
+	}
+
+	
+	/** Loads the client specification, unless overridden */
+	@Override
+	protected void loadServer(GameEntity entity)  {
+		
+	}
+	
+	@Override
+	public void runServer(GameEntity entity, float delta) {
+		setChanged();
+	}
+
+	@Override
+	public void runClient(GameEntity entity, float delta) {
+		if(this.snapshot != null) {
+			fill(this.snapshot);
+		}
+	}
+	
+	@Override
+	public void loadClient(GameEntity entity) {
+		this.body = entity.getWorld().createBody(createBodyDef());
+		
+		setUserdata(data.load(entity, this));
+		updateTransform(position.x, position.y, position.z, angle);
+		setDimension(dim);
+	}
+	
+	private BodyDef createBodyDef() {
+		BodyDef def = new BodyDef();
+		if(data.contains(UserData.D_BODY)) {
+			def.type = BodyType.valueOf((String)data.get(UserData.D_BODY));
+		} else {
+			def.type = BodyType.StaticBody;
+		}
+		return def;
+	}
+	
+	/** Will update the fixture for this {@link Box2DPhysics}, based on the {@link UserData} */
+	protected void updateFixture() {
+		// Create shape based on reflection
+		String type = (String) data.get(UserData.D_TYPE);
+		Shape shape = null;
+		if(type != null) {
+			if(type.equals(TYPE_CIRCLE)) {
+				shape = new CircleShape();
+				shape.setRadius(dim.getRadius());
+			} else if(type.equals(TYPE_RECT)) {
+				shape = new PolygonShape();
+				((PolygonShape) shape).setAsBox(dim.getWidth()*0.5f, dim.getHeight()*0.5f);
+			} else if(type.equals(TYPE_EDGE)) {
+				shape = new EdgeShape();
+				((EdgeShape) shape).set(new Vector2(position.x, position.y), new Vector2(position.x + dim.getWidth(), position.y + dim.getHeight()));
+			}
+			Fixture fix = body.createFixture(shape, 0.5f);
+			fix.setUserData(data);
+			
+			if(data.contains(UserData.D_SENSOR)) {
+				fix.setSensor((Boolean) data.get(UserData.D_SENSOR));
+			}
+			shape.dispose();
+			
+		} else {
+			System.err.println("NO SHAPE FOR " + type);
+		}
+	}
+	
+	/** Returns the angle of the body in radians */
+	public float getAngle() {
+		if(body != null) {
+			angle = body.getAngle();
+		}
+		return angle;
+	}
+	
+	/** Applies angular impulse to body */
+	public void rotateBy(float rot) {
+		if(this.body != null) {
+			this.body.applyAngularImpulse(rot, true);
+		}
+	}
+	
+	public void setDimension(Dimension dim) {
+		super.setDimension(dim);
+			
+		if(this.body != null) {
+			for (int i = 0; i < body.getFixtureList().size(); i++) {
+				body.destroyFixture(body.getFixtureList().get(i));
+			}
+			updateFixture();
+		}
+	}
+	
+	public Dimension getDimension() {
+		return this.dim;
+	}
+	
+	
+	/** Updates and returns the {@link Snapshot} for this component. */
+	@Override
+	public Snapshot getSnapshot(boolean delta) {
+		setSynced();
+		return delta ? get() : getFull();
+	}
+	
+	/** Update the position and angle for this body */
+	public void updateTransform(float x, float y, float z, float angle) {
+		// Could be null
+		if(this.body != null) {
+			this.body.setTransform(x, y, angle);
+		}
+		super.updateTransform(x, y, z, angle);
+	}
+	
+	/** Update the velocity for this body */
+	protected void updateVelocity(float x, float y, float z) {
+		if(this.body != null) {
+			this.body.setLinearVelocity(x, y);
+		}
+		super.updateVelocity(x, y, z);
+	}
+	
+	/** Update the velocity for this body */
+	public void updateImpulse(float x, float y, float z) {
+		if(this.body != null) {
+			this.body.applyLinearImpulse(x, y, body.getPosition().x, body.getPosition().y, true);
+		}
+		super.updateImpulse(x, y, z);
+	}
+	
+	/** Returns the current velocity for this component */
+	public Vector3 getVelocity() {
+		return body != null ? this.velocity.set(body.getLinearVelocity().x, body.getLinearVelocity().y, this.velocity.z) : super.getVelocity();
+	}
+	
+	/** Returns the current position for this component, or the initial position. **/
+	public Vector3 getPosition() {
+		return body != null ? this.position.set(body.getPosition().x, body.getPosition().y, this.position.z) : super.getPosition();
+	}
+	
+	public Snapshot get() {
+		snapshot.data = data.retrieve();
+		
+		return super.get();
+	}
+	
+	public Snapshot getFull() {
+		snapshot.data = data;
+		
+		return super.getFull();
+	}
+	
+	/**
+	 * Fills this Component with snapshot data from the server.
+	 * @param snapshot The data driven snapshot.
+	 * @return The updated {@link Component}.
+	 */
+	public Physics fill(Snapshot snapshot) {
+		if(snapshot.data != null) setUserdata(snapshot.data);
+		if(snapshot.v3_0 != null) this.updateTransform(snapshot.v3_0.x, snapshot.v3_0.y, snapshot.v3_0.z, angle);
+		if(snapshot.v3_1 != null) this.updateVelocity(snapshot.v3_1.x, snapshot.v3_1.y, snapshot.v3_1.z);
+		if(snapshot.v3_2 != null) this.updateImpulse(snapshot.v3_2.x, snapshot.v3_2.y, snapshot.v3_2.z);
+		if(snapshot.d_0 != null) setDimension(snapshot.d_0);
+		return this;
+	}
+	
+	@Override
+	public void destroy(GameEntity entity) {
+		entity.getWorld().destroyBody(body);
+	}
+	
+	public void setUserdata(UserData object) {
+		// Set
+		this.data = object;
+		this.snapshot.data = this.data;
+		
+		// Update
+		if(this.body != null) {
+			Filter filter = null;
+			if(data.contains(UserData.D_FILTER)) {
+				filter = (Filter) data.get(UserData.D_FILTER);
+			}
+			
+			for (int i = 0; i < body.getFixtureList().size(); i++) {
+				body.getFixtureList().get(i).setUserData(object);
+				if(filter != null) body.getFixtureList().get(i).setFilterData(filter);
+			}
+			this.body.setUserData(object);
+		}
+	}
+	
+	public UserData getUserdata() {
+		return this.data;
+	}
+	
+	/** Set a new filter for the fixtures. Will transmit changes */
+	public void setFilterData(Filter filter) {
+		this.data.put(UserData.D_FILTER, filter);
+		
+		if(this.body != null) {
+			for (int i = 0; i < body.getFixtureList().size(); i++) {
+				body.getFixtureList().get(i).setFilterData(filter);
+			}
+		}
+	}
+	
+	public Body getBody() {
+		return this.body;
+	}
+
+	public void setGravityZ(float grav) {
+		this.gravityZ = grav;
+		if(grav == 0) this.velocity.z = 0;
+	}
+	
+	public void setPosZ(float z) {
+		this.position.z = z;
+	}
+
+	public void setSensor(boolean b) {
+		if(this.body != null) {
+			for (int i = 0; i < body.getFixtureList().size(); i++) {
+				body.getFixtureList().get(i).setSensor(b);
+			}
+		}
+	}
+	
+	public void updateForceZ(float value) {
+		if(Math.abs(this.velocity.z) < 0.5f) {
+			this.velocity.z = value;
+			this.gravityZ = GRAV_Z;
+		}
+	}
+}
