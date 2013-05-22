097babfa8ba2db6744073e2a02c4fcebfad95a33
Kjetil Mehl <aspic@mehl.no>
2013-05-22 23:25:22 +0200

    Move in world space, removed camera hack.

--- src/no/mehl/component/renderer/DecalRenderer.java
+++ src/no/mehl/component/renderer/DecalRenderer.java
@@ -1,15 +1,22 @@
 package no.mehl.component.renderer;
 
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.decals.Decal;
+import com.badlogic.gdx.graphics.g3d.lights.PointLight;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Plane;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.Ray;
 
 import no.mehl.component.EntityManager;
 import no.mehl.component.GameEntity;
 import no.mehl.component.Physics;
 import no.mehl.component.Renderer;
 import no.mehl.component.Snapshot;
+import no.mehl.libgdx.ui.UIManager;
 import no.mehl.libgdx.utils.ShaderManager;
 
 public class DecalRenderer extends Renderer {
@@ -17,6 +24,7 @@ public class DecalRenderer extends Renderer {
 	protected Decal decal;
 	protected Physics physics;
 	private float rotationY;
+	private PointLight light = new PointLight();
 	
 	public DecalRenderer() {
 		this("staticSpaceObject");
@@ -29,10 +37,13 @@ public class DecalRenderer extends Renderer {
 	@Override
 	public void loadClient(GameEntity entity) {
 		physics = entity.getExtends(Physics.class);
-		
 		if(physics != null) {
+			System.out.println(physics.getDimension());
 			decal = Decal.newDecal(physics.getDimension().width, physics.getDimension().height, EntityManager.assets.get(key), true);
 			if(color != null) decal.setColor(color.r, color.g, color.b, color.a);
+			light.color.set(Color.WHITE);
+			light.intensity = 20f;
+			ShaderManager.getInstance().getLights().add(light);
 		}
 	}
 	
@@ -46,7 +57,9 @@ public class DecalRenderer extends Renderer {
 	public void runServer(GameEntity entity, float delta) {
 		
 	}
-
+	
+	float dist = 7f;
+	
 	@Override
 	public void runClient(GameEntity entity, float delta) {
 		if(physics != null) {
@@ -61,8 +74,15 @@ public class DecalRenderer extends Renderer {
 			
 			ShaderManager.getInstance().getDecalBatch().add(decal);
 			if(follow) {
-				ShaderManager.getInstance().translate(position.x, position.y, position.z + physics.getDimension().depth);
-				ShaderManager.getInstance().keepZDistance(position.z);
+				
+				ShaderManager.getInstance().getCamera().position.set(position.x, position.y - dist, 5f);
+				ShaderManager.getInstance().getCamera().lookAt(position.x, position.y + dist, 0);
+//				ShaderManager.rotatePoint(ShaderManager.getInstance().getCamera().position, position, physics.getAngle()*MathUtils.radDeg);
+//				ShaderManager.getInstance().keepZDistance(position.z);
+				float x = MathUtils.sin(physics.getAngle());
+				float y = MathUtils.cos(physics.getAngle());
+				
+				light.position.set(position.x + 2*x, position.y + 2*y, 0);
 			}
 		}
 	}
